f8ef748a12a9f989cfaf1da756e24c638ee9ecdb
martinyounghoonkim <hoons0131@gmail.com>
2021-11-09 21:04:38 +0900

    Add project

---
+++ src/main/java/com/example/javaspringbootjunitplayground/JavaSpringbootJunitPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootjunitplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootJunitPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringbootJunitPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaspringbootjunitplayground/JavaSpringbootJunitPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootjunitplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootJunitPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
