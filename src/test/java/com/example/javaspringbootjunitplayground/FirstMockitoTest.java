f2b57960b2d8c3684a2cb86c7f7cbcddb489b441
martinyounghoonkim <hoons0131@gmail.com>
2021-11-22 23:46:16 +0900

    Setup class for testing mockito

---
+++ src/test/java/com/example/javaspringbootjunitplayground/FirstMockitoTest.java
@@ -0,0 +1,13 @@
+package com.example.javaspringbootjunitplayground;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstMockitoTest {
+
+    @Test
+    void test() {
+        assertTrue(true);
+    }
+}
